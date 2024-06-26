ea9577dde135ce3215e283794f9f9e968094314e
Soumyajit Das <94384694+Soumyajit9696@users.noreply.github.com>
2024-06-26 16:37:23 +0000

    finish

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 98978
+let accountEmail ="sjd@dji.com"
+var passWord = "123456"
+accountCity = "Mahammadpur"
+// console.log(accountId);
+let accountState;
+
+/*
+please dont use var for variables,
+because of issue in block scope and functional scope.
+*/
+
+
+console.table([accountId, accountEmail,passWord,accountCity, accountState])
